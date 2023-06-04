7a6d0bfa32d42aca2ad2cf909e0056953b8e7a53
sangmin <sangmin4208@gmail.com>
2023-06-04 11:35:42 +0900

    🎨 useState Hooks & Mapping Through Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { createRoot } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import Pet from './components/Pet'
-
+import SearchParams from './components/SeachParams'
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
+      <div>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="Cat" breed="Mixed" />
+      </div>
     </div>
   )
 }
---
+++ src/components/SeachParams.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
+
+const SearchParams = () => {
+  const [location, setLocation] = useState('Seattle, WA')
+  const [animal, setAnimal] = useState('')
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label>
+          Animal
+          <select value={animal} onChange={(e) => setAnimal(e.target.value)}>
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+        <button type="reset">reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchParams
