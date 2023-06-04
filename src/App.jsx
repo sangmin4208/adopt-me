60f69d9e79e65568579bc41cc15daafe4f0baf53
SangMin <sangmin4208@gmail.com>
2023-06-04 11:03:37 +0900

    🎨 configure ESLint & React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom'
+import Pet from './components/Pet'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  )
+}
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
