396aeb77d9387e625714759a349e70ae97a4c157
SangMin <sangmin4208@gmail.com>
2023-06-07 11:53:12 +0900

    ✨ Organize folder

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,27 @@ import Details from './pages/Details'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/query-client'
 import App from './App'
+import Home from './pages/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/details/:id',
-    element: <Details />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/details/:id',
+        element: <Details />,
+      },
+    ],
   },
 ])
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <div>
-      <h1>Adopt Me!</h1>
-    </div>
     <RouterProvider router={router} />,
   </QueryClientProvider>,
 )
