f5aaa39c5d126caae51e6a4dcda05dea6d4d251e
SangMin <sangmin4208@gmail.com>
2023-06-07 13:20:17 +0900

    🎨 Modals with Portals

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,13 +1,14 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import fetchPet from '../services/fetchPet'
 import { useQuery } from '@tanstack/react-query'
 import Carousel from '../components/Carousel'
 import ErrorBoundary from '../components/ErrorBoundary'
-
+import Modal from '../components/Modal'
 const Details = () => {
+  const [showModal, setShowModal] = useState(false)
   const { id } = useParams()
   const { data, isError, isLoading } = useQuery(['details', id], fetchPet)
-
   if (isLoading) {
     return (
       <div className="loading-pane">
@@ -22,17 +23,31 @@ const Details = () => {
 
   const pet = data.pets[0]
   return (
-    <div className="details">
-      <Carousel images={pet.images} />
-      <div>
-        <h1>{pet.name}</h1>
-        <h2>
-          {`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}
-          <button>Adopt {pet.name}</button>
-          <p>{pet.description}</p>
-        </h2>
+    <>
+      {showModal && (
+        <Modal>
+          <div>
+            <h1>Would you like to adopt {pet.name}?</h1>
+            <div className="buttons">
+              <button onClick={() => setShowModal(false)}>Yes</button>
+              <button onClick={() => setShowModal(false)}>No</button>
+            </div>
+          </div>
+        </Modal>
+      )}
+
+      <div className="details">
+        <Carousel images={pet.images} />
+        <div>
+          <h1>{pet.name}</h1>
+          <h2>
+            {`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}
+            <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+            <p>{pet.description}</p>
+          </h2>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
